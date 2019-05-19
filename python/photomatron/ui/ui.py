6ed22e78a481a0b4d57b3625a22ee6c006f29931
mrfrangipane <mrfrangipane@frangi.pane>
2019-05-19 16:50:23 +0200

    feat(app) : font stretching

--- python/photomatron/ui/ui.py
+++ python/photomatron/ui/ui.py
@@ -2,6 +2,21 @@ from PySide import QtGui
 from PySide import QtCore
 
 
+FONT_STRETCH = 0.72
+
+
+class Fonts:
+    def __init__(self):
+        self.title = QtGui.QFont('Coolvetica Rg', 32)
+        self.title.setStretch(FONT_STRETCH * 100)
+
+        self.message = QtGui.QFont('Open Sans', 16)
+        self.message.setStretch(FONT_STRETCH * 100)
+
+        self.buttons = QtGui.QFont('Coolvetica Rg', 20)
+        self.buttons.setStretch(FONT_STRETCH * 100)
+
+
 class CameraOverlayPlaceholder(QtGui.QLabel):
     def __init__(self, parent=None):
         QtGui.QLabel.__init__(self, parent)
@@ -14,19 +29,21 @@ class Ui(QtGui.QWidget):
     def __init__(self, parent=None):
         QtGui.QWidget.__init__(self, parent)
 
+        self.fonts = Fonts()
+
         self.camera_placeholder = CameraOverlayPlaceholder()
         self.camera_placeholder.setFixedSize(475, 475)
 
         self.label_tile = QtGui.QLabel("Photomatron")
         self.label_tile.setFixedHeight(80)
+        self.label_tile.setFont(self.fonts.title)
 
         self.label_message = QtGui.QLabel("This is a message from the Queen of Great Britain : Keep calm and take a picture")
         self.label_message.setWordWrap(True)
-        self.label_message.setFont(QtGui.QFont('Open Sans', 16))
-        self.label_tile.setFont(QtGui.QFont('Coolvetica Rg', 32))
+        self.label_message.setFont(self.fonts.message)
 
         self.buttons_guide = QtGui.QLabel("Prev | OK | Next")
-        self.buttons_guide.setFont(QtGui.QFont('Coolvetica Rg', 20))
+        self.buttons_guide.setFont(self.fonts.buttons)
         self.buttons_guide.setFixedHeight(80)
 
         self.layout_ = QtGui.QGridLayout(self)
