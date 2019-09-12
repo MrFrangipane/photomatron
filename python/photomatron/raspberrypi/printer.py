8a44709b79179acc6e33146dc2cb43ceada4993c
mrfrangipane <mrfrangipane@frangi.pane>
2019-09-12 17:26:10 +0200

    feat(printer): save job attribute to json file

--- python/photomatron/raspberrypi/printer.py
+++ python/photomatron/raspberrypi/printer.py
@@ -1,10 +1,14 @@
+import json
 import cups
 
 
 class Printer:
+    conn = cups.Connection()
 
     def print_picture(self, filepath, printer_name):
         print("Printer : '{}' prints {}".format(printer_name, filepath))
 
-        conn = cups.Connection()
-        conn.printFile(printer_name, filepath, 'Photomatron', {})
+        job_id = Printer.conn.printFile(printer_name, filepath, 'Photomatron', {})
+
+        with open(filepath + '.json', 'w+') as f_job:
+            json.dump(Printer.conn.getJobAttributes(job_id), f_job, indent=2)
